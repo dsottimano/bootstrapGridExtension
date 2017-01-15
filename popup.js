a6133325e745c8918ea1b21bcf0a7d8788092eb2
dsottimano <dsottimano@gmail.com>
2017-01-15 11:37:30 -0700

    version: 0.1.2

--- popup.js
+++ popup.js
@@ -1,15 +1,21 @@
-
 var html = document.createElement('div');
-    html.innerHTML = '<input id="clickMe" type="button" value="TOGGLE GRID" style="position: fixed;bottom: 10px;right: 5px;background-color: #e7e7e7 !important; color: black !important;" />';
-    document.body.appendChild(html);
+html.innerHTML = '<input id="clickMe" type="button" value="TOGGLE GRID" style="position: fixed;bottom: 10px;right: 5px;background-color: #e7e7e7 !important; color: black !important;" />';
+document.body.appendChild(html);
 
-$("<style type='text/css'> .btn-122211221276{;} .greenRowHelper{ background-color:green !important;} .redRowHelper{ background-color:red !important;border: 4px solid purple !important;} .blueRowHelper{ background-color:blue !important;}</style>").appendTo("head");
+$("<style type='text/css'> .textHelperPosition {display:none;margin-left:0px !important;margin-top:0px !important; z-index: 1 !important; color:white;} .greenRowHelper{ background-color:green !important;} .redRowHelper{ background-color:red !important;border: 4px solid purple !important;} .blueRowHelper{ background-color:blue !important;}</style>").appendTo("head");
+
+//Inject the labels into the DOM as display none
+$("div.container").append('<p class="textHelperPosition"><strong>***CONTAINER***</strong></p>');
+$("div.row").append('<p class="textHelperPosition"><strong>***ROW***</strong></p>');
+$("div[class^='col']").append('<p class="textHelperPosition"><strong>***COLUMN***</strong></p>');
 
 
+//When button is clicked, toggle the display of colors and text
 $("#clickMe").click(function () {
 
- $("div.container").toggleClass("blueRowHelper");
- $("div.row").toggleClass("greenRowHelper");
- $("div[class^='col']").toggleClass("redRowHelper");
+    $("div.container").toggleClass("blueRowHelper");
+    $(".textHelperPosition").toggle();
+    $("div.row").toggleClass("greenRowHelper");
+    $("div[class^='col']").toggleClass("redRowHelper");
 
 });
